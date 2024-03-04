38e1b021e2ced71a70d633120b8a063272579316
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-03-04 13:34:32 +0330

    Updated Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,12 @@
 package Main;
 import App.Application;
+import data.Users;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		Users.populate_users();
 		Application app = new Application();
 		app.run();
 	}
