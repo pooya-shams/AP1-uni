df4ec27dc15432a4c80f41e1ae9e88f2b05bfa55
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-03-05 16:35:49 +0330

    Added get_dep_code in IOHelper

--- src/util/IOHelper.java
+++ src/util/IOHelper.java
@@ -1,5 +1,8 @@
 package util;
 
+import data.Departments;
+import university.Department;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintStream;
@@ -36,10 +39,31 @@ public class IOHelper
 		mf = sc.nextLine().trim();
 		while( ! (IOHelper.is_numeric(mf) && IOHelper.in_range(IOHelper.str_to_int(mf), 0, max_opt) ) )
 		{
+			System.out.println("valid options are numbers 0 through "+max_opt);
+			System.out.print("enter a real value: ");
+			mf = sc.nextLine().trim();
+		}
+		return str_to_int(mf);
+	}
+	public static boolean is_real_dep_code(int n)
+	{
+		if(n == 0) // special case
+			return true;
+		for(Department dep: Departments.deps)
+		{
+			if(dep.getCode() == n)
+				return true;
+		}
+		return false;
+	}
+	public static int get_dep_code(Scanner sc)
+	{
+		System.out.print("choose department by code (or 0 to go back): ");
+		String mf = sc.nextLine().trim();
+		while( ! (IOHelper.is_numeric(mf) && IOHelper.is_real_dep_code(IOHelper.str_to_int(mf)) ) )
+		{
 			System.out.println("your input '"+mf+"' is not a valid option");
-			System.out.print("valid options are numbers 0 through "+max_opt);
-			System.out.println();
-			System.out.println("enter a real value: ");
+			System.out.print("please enter a real department code or 0: ");
 			mf = sc.nextLine().trim();
 		}
 		return str_to_int(mf);
