49e45123354882f80e55d8e4846a6c31972cb97e
Pooya Shams Kolahi <pooya.shams.k@gmail.com>
2024-03-08 13:23:32 +0330

    Added the sing up functionality to Application

--- src/App/Application.java
+++ src/App/Application.java
@@ -1,5 +1,6 @@
 package App;
 
+import Exceptions.InvalidCode;
 import data.Users;
 import university.Student;
 import university.User;
@@ -20,20 +21,40 @@ public class Application
 		System.out.println("you can always go back by typing 0");
 		while(true)
 		{
-			if(this.general_login())
+			System.out.println(
+				"[0] Exit\n"+
+				"[1] login (I already have an account)\n"+
+				"[2] sign up (I don't have an account)"
+			);
+			int type = IOHelper.get_valid_input(2, sc);
+			if(type == 0)
+			{
+				System.out.println("See you next time!");
 				return;
+			}
+			else if(type == 1)
+			{
+				general_login();
+			}
+			else if(type == 2)
+			{
+				sign_up();
+			}
+			else
+			{
+				System.out.println("unreachable statement no. 29281821");
+			}
 		}
 	}
 	private boolean general_login() // returns if we should die
 	{
 		System.out.println("Choose your login type:\n"+
-			"[0] Exit\n" +
+			"[0] go back\n" +
 			"[1] Student\n"+
 			"[2] Admin");
 		int type = IOHelper.get_valid_input(2, sc); // I fucking hate java you stupid piece of shit
 		if(type == 0)
 		{
-			System.out.println("See you next time!");
 			return true;
 		}
 		else if(type == 1)
@@ -49,6 +70,24 @@ public class Application
 			System.err.println("nope not possible");
 		}
 		return false;
+	}
+	private void sign_up()
+	{
+		System.out.println("please enter your student id. it will be used as a username.");
+		System.out.println("please note that it can't contain whitespace and must contain only digits");
+		String username = sc.nextLine();
+		System.out.println("now please enter your password: ");
+		String password = sc.nextLine();
+		try
+		{
+			Student st = new Student(username, password);
+			Users.add_user(st);
+		}
+		catch (InvalidCode e)
+		{
+			System.out.println("[ERROR] you entered an invalid student code. aborting");
+			System.out.println(e.getMessage());
+		}
 	}
 	private void student_login() // oh and also camel case is nonsense
 	{
